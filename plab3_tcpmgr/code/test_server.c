0a768062dd962918b8e1a491cbf412220e8f2bee
Simeon <simeon.serafimov@student.kuleuven.be>
2023-12-22 10:51:20 +0100

    final version lab assignments

---
+++ plab3_tcpmgr/code/test_server.c
@@ -0,0 +1,98 @@
+/**
+ * \author {AUTHOR}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "config.h"
+#include "lib/tcpsock.h"
+#include <pthread.h>
+
+/**
+ * Implements a sequential test server (only one connection at the same time)
+ */
+
+void *client_handler(tcpsock_t *client) {
+    int bytes, result;
+    sensor_data_t data;
+
+    do { // reading sensor data
+        bytes = sizeof(data.id);
+        result = tcp_receive(client, (void *) &data.id, &bytes);
+        bytes = sizeof(data.value);
+        result = tcp_receive(client, (void *) &data.value, &bytes);
+        bytes = sizeof(data.ts);
+        result = tcp_receive(client, (void *) &data.ts, &bytes);
+        if ((result == TCP_NO_ERROR) && bytes) {
+            printf("sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", data.id, data.value,
+                    (long int) data.ts);
+        }
+    } while (result == TCP_NO_ERROR);
+
+    if (result == TCP_CONNECTION_CLOSED)
+        printf("Peer has closed connection\n");
+    else
+        printf("Error occurred on connection to peer\n");
+
+    if (tcp_close(&client) != TCP_NO_ERROR) {
+        fprintf(stderr, "Error closing client socket\n");
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+
+    // main process creating server socket and threads for each client.
+    /*server socket creation*/
+    tcpsock_t *server, *client;
+    int conn_counter = 0;
+
+    if(argc < 3) {
+    	printf("Please provide the right arguments: first the port, then the max nb of clients");
+    	return -1;
+    }
+    int MAX_CONN = atoi(argv[2]);
+    int PORT = atoi(argv[1]);
+
+    printf("Test server is started\n");
+    if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) {
+        fprintf(stderr, "error during socket creation of server");
+        exit(EXIT_FAILURE);
+
+    }
+
+    /*thread creation for each client*/
+    pthread_t thread_ids[MAX_CONN];
+
+    do {
+        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) {
+            fprintf(stderr, "error client listening: either, the port isn't correctly assigned, malloc error or socket operation error");
+            exit(EXIT_FAILURE);
+        }
+        printf("Incoming client connection\n");
+
+
+
+        if (pthread_create(thread_ids + conn_counter, NULL, (void*)client_handler, client) != 0) {
+            fprintf(stderr, "Error during thread creation for client.");
+            exit(EXIT_FAILURE);
+        }
+        conn_counter++;
+
+    } while (conn_counter < MAX_CONN);
+
+    for (int i = 0; i < MAX_CONN; i++) {
+        if (pthread_join(thread_ids[i], NULL) != 0) {
+            fprintf(stderr, "Error joining thread\n");
+        }
+    }
+    if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    printf("Test server is shutting down\n");
+    return 0;
+}
+
+
+
+
---
+++ plab_4_sbuffer/main.c
@@ -0,0 +1,170 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include "sbuffer.h"
+
+#define NUM_OF_THREADS 7
+
+// global variables
+sbuffer_t *buffer;
+FILE *sensor_file_out;
+pthread_mutex_t reader_mutex;
+
+int writer_routine() {
+    // opening file to read from
+    FILE * sensor_file_in = fopen("sensor_data", "r");
+    if (sensor_file_in == NULL) {
+        perror("Error occurred during opening read file");
+        exit(EXIT_FAILURE);
+    }
+    sensor_data_t sensor_node;
+    do {
+        // read the data
+        size_t read1 = fread(&(sensor_node.id), sizeof(sensor_id_t), 1, sensor_file_in);
+        if (read1 == 0) {
+            if (feof(sensor_file_in)) {
+                break;
+            } else {
+                perror("Error reading sensor_id from file");
+                fprintf(stderr, "Stopping execution of program");
+                exit(EXIT_FAILURE);
+            }
+        } else if (read1 != 1) {
+            perror("Error reading sensor_id from file");
+            fprintf(stderr, "Stopping execution of program");
+            exit(EXIT_FAILURE);
+        }
+
+        size_t read2 = fread(&sensor_node.value, sizeof(sensor_value_t), 1, sensor_file_in);
+        if (read2 != 1) {
+            perror("Error reading temperature from file");
+            fprintf(stderr, "Stopping execution of program");
+            exit(EXIT_FAILURE);
+        }
+
+        size_t read3 = fread(&(sensor_node.ts), sizeof(sensor_ts_t), 1, sensor_file_in);
+        if (read3 != 1) {
+            perror("Error reading timestamp from file");
+            fprintf(stderr, "Stopping execution of program");
+            exit(EXIT_FAILURE);
+        }
+
+        // writing data to buffer
+        printf("writing\n");
+        if (sbuffer_insert(buffer, &sensor_node) == SBUFFER_FAILURE) {
+            perror("Error occurred during insertion of data into buffer");
+            exit(EXIT_FAILURE);
+        }
+        usleep(10000);
+    } while(1);
+    fclose(sensor_file_in);
+    sensor_node.id = 0;
+    sensor_node.value = 0;
+    sensor_node.ts = 0;
+    printf("\n\n\nWRITER IS DONE\n\n\n");
+    for (int i = 0; i < NUM_OF_THREADS ; i++) { // posting such that the other threads can also leave the routine.
+        if (sbuffer_insert(buffer, &sensor_node) == SBUFFER_FAILURE) {
+            perror("Error occurred during insertion of data into buffer");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+bool is_end_buffer;
+int reader_routine() {
+//     reading data from buffer + writing to file
+    sensor_data_t data;
+    do{
+        // reading from buffer
+        int result = sbuffer_remove(buffer, &data);
+//        pthread_mutex_lock(&reader_mutex);
+        if ( result == SBUFFER_FAILURE) {
+            perror("Error occurred during removal of data from buffer");
+            exit(EXIT_FAILURE);
+        } else if (result == SBUFFER_NO_DATA) {
+             is_end_buffer = true;
+            break;
+        }
+//        pthread_mutex_unlock(&reader_mutex);
+        printf("%lu reading from buffer: %d, %f, %s", pthread_self(), data.id, data.value, ctime(&data.ts));
+        if (pthread_mutex_lock(&reader_mutex) != 0) {
+            perror("Critical error during locking mutex");
+            exit(EXIT_FAILURE);
+        }
+        if (data.id != 0) {
+            if (fprintf(sensor_file_out, "%d,%f,%lu\n", data.id, data.value, data.ts) < 0) {
+                perror("Error during writing to output file");
+                pthread_mutex_unlock(&reader_mutex);
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (pthread_mutex_unlock(&reader_mutex) != 0) {
+            perror("Critical error unlocking mutex");
+            exit(EXIT_FAILURE);
+        }
+        usleep(25000);
+
+        if (is_end_buffer == true) {
+            break;
+        }
+    } while(1);
+    printf("\n\n\nreading finished\n\n\n");
+    return 0;
+}
+
+int main() {
+    // Buffer initialization
+    if (sbuffer_init(&buffer) == SBUFFER_FAILURE) {
+        perror("Error occurred during initialization of buffer");
+        return EXIT_FAILURE;
+    }
+
+    // Initialize mutex
+    if (pthread_mutex_init(&reader_mutex, NULL) != 0) {
+        perror("Mutex initialization failed \n");
+        exit(EXIT_FAILURE);
+    }
+
+    // opening writing sensor file
+    sensor_file_out = fopen("sensor_data_out.csv", "w");
+    if (sensor_file_out == NULL) {
+        perror("Error opening sensor_data_out.csv");
+        exit(EXIT_FAILURE);
+    }
+
+    //threads creation
+    pthread_t th[NUM_OF_THREADS];
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        if (i == 0) {
+            if (pthread_create(th, NULL, (void *)writer_routine, NULL) != 0) {
+                perror("Writer thread creation failed");
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+        else {
+            if (pthread_create(th + i, NULL, (void *)reader_routine, NULL) != 0){
+                perror("Reader thread creation failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    // waiting for threads to finish
+    for (int i = 0; i < NUM_OF_THREADS; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Error joining thread.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // freeing resources
+    if (sbuffer_free(&buffer) == SBUFFER_FAILURE) { // NO CRITICAL ERROR!
+        perror("Error occurred during freeing of the resources");
+    }
+    fclose(sensor_file_out);
+    return 0;
+}
